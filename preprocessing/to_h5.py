70939dbbb27e805b014173bc7ca958146e3b843b
Victor Kozhevnikov <70488161+spaiker7@users.noreply.github.com>
2023-03-15 22:37:18 +0300

    Create to_h5.py

---
+++ preprocessing/to_h5.py
@@ -0,0 +1,12 @@
+import h5py
+import numpy as np
+
+# use path to atchived h5
+h5_file = h5py.File('C:/Users/spaik/Documents/lab/ML_project/dense-162dip_parcell-64_GRID-64_paired_scale-False-2000.h5', 'r')
+dataset = h5_file.get('dense-162dip_parcell-64_GRID-64_paired_scale-False-2000')
+storage = np.array(dataset)
+
+# create unarchived h5 file
+hf_unarchived = h5py.File('dense-162dip_parcell-64_GRID-64_paired_scale-False-2000_unarch.h5', 'w')
+hf_unarchived.create_dataset('dense-162dip_parcell-64_GRID-64_paired_scale-False-2000', data=storage)
+hf_unarchived.close()
