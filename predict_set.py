b87c589e39a6a4fbf6f6405e72a1b75adafee4a9
Andrei Kalinichenko <122077162+AndreiKalinichenko@users.noreply.github.com>
2023-03-16 09:16:41 +0300

    Update predict_set.py
    corrected for h5_dataset

--- predict_set.py
+++ predict_set.py
@@ -9,15 +9,12 @@ from torch.utils.data import Dataset
 
 
 from models import UNet3D, VNet
-from CustomImageDataset import CustomImageDataset
+from CustomImageDataset import h5_dataset
 
 DATE_FORMAT = '%Y-%m-%d_%H-%m-%S'
 
 def predict_set(model, model_name, out_dir, data_loader=None, label=None, set_size=10, data_limit=800,
-                predict_only=False, img_dir=None, label_dir=None, train_part=None):
-    '''
-    for predict only write img_dir, label_dir, train_part into *dirs
-    '''
+                predict_only=False, h5_file_path=None, train_part=None):
 
     def predict(data_loader, model, label, out_dir):
         with torch.no_grad():
@@ -27,33 +24,17 @@ def predict_set(model, model_name, out_dir, data_loader=None, label=None, set_si
                 np.savez(os.path.join(out_dir, f'{label}_{i}_pred'), input.cpu().numpy(), pred.cpu().numpy(), y_true)
                 if i == set_size: break
 
-    def create_loaders(img_dir, label_dir, train_part):
-        dataset_input_storage = []
-        dataset_label_storage = []
-        
-        random.seed(10)
-        counter = 0
-        while counter < data_limit:
-            img, label = random.choice(list(zip(os.listdir(img_dir), os.listdir(label_dir))))
-            with np.load(os.path.join(img_dir, img)) as img:
-                image = np.expand_dims(img['arr_0'], axis=0)
-                dataset_input_storage.append(image)
-            with np.load(os.path.join(label_dir, label)) as label:
-                image = np.expand_dims(label['arr_0'], axis=0)
-                dataset_label_storage.append(image)
-            counter +=1
-        generator = torch.Generator() 
-        generator.manual_seed(0)
+    def create_loaders(h5_file_path, train_part):
 
-        dataset = CustomImageDataset(dataset_input_storage, dataset_label_storage)
-        train_part = round(len(dataset_input_storage) * train_part)
+        dataset = h5_dataset(h5_file_path)
+        train_part = round(len(dataset) * train_part)
         train_dataset, test_dataset = torch.utils.data.random_split(dataset,
-                                                                    [train_part, len(dataset_input_storage) - train_part],
+                                                                    [train_part, len(dataset) - train_part],
                                                                     generator=generator)
-        
-        train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-        test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
-        return  train_dataloader, test_dataloader
+
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+        return train_dataloader, test_dataloader
 
     if model == 'Unet3D':
         model = UNet3D(1, 1, final_sigmoid=False, f_maps=64, layer_order='cr',
@@ -66,7 +47,7 @@ def predict_set(model, model_name, out_dir, data_loader=None, label=None, set_si
     model = model.to(torch.device('cuda:0'))
 
     if predict_only:
-        train_dataloader, test_dataloader = create_loaders(img_dir, label_dir, train_part)
+        train_dataloader, test_dataloader = create_loaders(h5_file_path, train_part)
         predict(train_dataloader, model, 'train', out_dir)
         predict(test_dataloader, model, 'test', out_dir)
     else:
