79c9fcf46af18644c4d57ee2c49773d4b6c84861
Andrei Kalinichenko <122077162+AndreiKalinichenko@users.noreply.github.com>
2023-03-16 09:11:11 +0300

    Update CustomImageDataset.py

--- CustomImageDataset.py
+++ CustomImageDataset.py
@@ -18,3 +18,23 @@ class CustomImageDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
+    
+    
+    
+class h5_dataset(Dataset):
+    def __init__(self, h5_file_path):
+        self.h5_file = h5py.File(h5_file_path, 'r')
+        self.dataset = self.h5_file.get(list(self.h5_file.keys())[0])
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+
+        electrodes = self.dataset[idx, 0]
+        dipoles = self.dataset[idx, 1]
+
+        electrodes = np.expand_dims(electrodes, axis=0)
+        dipoles = np.expand_dims(dipoles, axis=0)
+
+        return electrodes, dipoles
