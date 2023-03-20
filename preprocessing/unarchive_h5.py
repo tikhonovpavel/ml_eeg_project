ee129c4818688f064a5e4f9b4281f1191077bde1
Pavel <pav3370@gmail.com>
2023-03-20 09:34:45 +0300

    unarchive h5 dataset by chunks

--- preprocessing/unarchive_h5.py
+++ preprocessing/unarchive_h5.py
@@ -1,19 +1,33 @@
 import h5py
-import numpy as np
 import argparse
+import numpy as np
 
-# create argument parser to accept filename
+
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--filename", help="path to archived h5 file")
+parser.add_argument("--chunk_size", default=500)
 args = parser.parse_args()
 
-# use path to archived h5
 h5_file = h5py.File(args.filename, 'r')
 dataset = h5_file.get('dense-162dip_parcell-64_GRID-64_paired_scale-False-2000')
-storage = np.array(dataset)
 
-# create unarchived h5 file
+chunk_size = 500
+nrows = dataset.shape[0]
+nchunks = (nrows - 1) // chunk_size + 1
+
 unarchived_file_name = args.filename.replace('.h5', '_unarch.h5')
 hf_unarchived = h5py.File(unarchived_file_name, 'w')
-hf_unarchived.create_dataset('dense-162dip_parcell-64_GRID-64_paired_scale-False-2000', data=storage)
+
+unarchived_dataset = hf_unarchived.create_dataset('dense-162dip_parcell-64_GRID-64_paired_scale-False-2000',
+                                                  shape=dataset.shape, dtype=dataset.dtype)
+
+for i in range(nchunks):
+    start = i * chunk_size
+    end = min(start + chunk_size, nrows)
+    chunk = dataset[start:end]
+    unarchived_dataset[start:end] = chunk
+
+h5_file.close()
 hf_unarchived.close()
