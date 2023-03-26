4ac78fbe9a68baab050b0dbe463eddd44c8899a7
Victor Kozhevnikov <70488161+spaiker7@users.noreply.github.com>
2023-03-26 21:20:50 +0300

    Update CustomImageDataset.py

--- CustomImageDataset.py
+++ CustomImageDataset.py
@@ -2,7 +2,6 @@ from torch.utils.data import Dataset
 import h5py
 import numpy as np
 
-
 class CustomImageDataset(Dataset):
     def __init__(self, dataset_input_storage, dataset_label_storage, transform=None, target_transform=None):
         self.dataset_input_storage = dataset_input_storage
@@ -21,19 +20,26 @@ class CustomImageDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
-
-
-class H5Dataset(Dataset):
+    
+    
+    
+class h5_dataset(Dataset):
     def __init__(self, h5_file_path):
-        self.h5_file = h5py.File(h5_file_path, 'r')
-        self.dataset = self.h5_file.get(list(self.h5_file.keys())[0])
+        self.data = h5py.File(h5_file_path, 'r')
+        self.keys = list(self.data.keys())
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.data)
 
-    def __getitem__(self, idx):
-        electrodes = self.dataset[idx, 0]
-        dipoles = self.dataset[idx, 1]
+    def __getitem__(self, index):
+        
+
+        key = self.keys[index]
+        sample = np.array(self.data[key])
+
+        electrodes = sample[0]
+        dipoles = sample[1]
+        dipoles[dipoles > 0] = 1
 
         electrodes = np.expand_dims(electrodes, axis=0)
         dipoles = np.expand_dims(dipoles, axis=0)
